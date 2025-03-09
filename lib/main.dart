f78b77bf8967ee3bf7134599425fdb978e1c1d80
sashaghadigaonkar <sashagladdy11@gmail.com>
2025-03-10 03:49:21 +0530

    seconf commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,20 +31,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+
+  var result ="";
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          // the App.build method, and use it to set our appbar title.
-          title: Text('flutter container'),
+          title: Text('BMI Calc.'),
         ),
-        body: Center(
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-            child: Text('hello world'),
-          ),
+        body: Container(
+          width: 300,
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Text("BMI Calci", style: TextStyle(fontSize: 34, fontWeight: FontWeight.w600),),
+
+            SizedBox(height: 21,),
+
+            TextField(
+              controller: wtController,
+              decoration: InputDecoration(
+                label:  Text('enter weight in kgs'),
+                prefixIcon: Icon(Icons.line_weight)
+              ),
+              keyboardType: TextInputType.number,
+            ),
+
+            SizedBox(height: 11),
+
+            TextField(
+              controller: ftController,
+              decoration: InputDecoration(
+                label:  Text('enter height in ft.'),
+                prefixIcon: Icon(Icons.line_weight)
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 11),
+
+
+            TextField(
+              controller: inController,
+              decoration: InputDecoration(
+                label:  Text('enter height in ft.'),
+                prefixIcon: Icon(Icons.line_weight)
+              ),
+              keyboardType: TextInputType.number,
+            ),
+
+            ElevatedButton(onPressed:(){
+              var wt = wtController.text.toString();
+              var ft = ftController.text.toString();
+              var inch = inController.text.toString();
+
+              if(wt!= "" && ft!= "" && inch!= ""){
+
+
+                var iWt = int.parse(wt);
+                var iFt = int.parse(ft);
+                var iInch = int.parse(inch);
+
+                var tInch = (iFt*12) + iInch;
+                var tCm = tInch*2.54;
+                var tM = tCm/100;
+
+                var bmi = iWt/(tM*tM);
+
+                setState(() {
+                  result = "your bmi is : ${bmi.toStringAsFixed(2)}";
+                });
+
+                
+
+              } else {
+                setState((){
+                  result = "fill everything";
+                });
+
+              }
+            }, child: Text('calculate')),
+
+            Text(result, style: TextStyle(
+              fontSize: 19
+            ),)
+
+
+        ],)
         ));
   }
 }
